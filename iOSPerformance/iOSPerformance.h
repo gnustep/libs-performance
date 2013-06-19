4f56df14454bfa673c78175d0474887509bcdd89
Niels Grewe <thebeing+gna@halbordnung.de>
2013-06-19 02:37:23 +0000

    Improve table views, fix for missing Unicode support on iOS 5.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/performance/trunk@36730 72102866-910b-0410-8b05-ffd578937521

---
+++ iOSPerformance/iOSPerformance.h
@@ -0,0 +1,13 @@
+//
+//  iOSPerformance.h
+//  iOSPerformance
+//
+//  Created by Niels Grewe on 17.05.13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSPerformance : NSObject
+
+@end
---
+++ iOSPerformance/iOSPerformance.m
@@ -0,0 +1,13 @@
+//
+//  iOSPerformance.m
+//  iOSPerformance
+//
+//  Created by Niels Grewe on 17.05.13.
+//
+//
+
+#import "iOSPerformance.h"
+
+@implementation iOSPerformance
+
+@end
